daa74881b2c4914d9279553cf2c4936f1bdd578e
Veronidas <veronabrkich@gmail.com>
2024-01-29 19:07:46 -0300

    Add multifiles configurations

---
+++ devops.aws/src/test/java/com/devops/aws/externalproptest/ExternaPropertyMultiFileTest.java
@@ -0,0 +1,30 @@
+package com.devops.aws.externalproptest;
+
+import com.devops.aws.jms.FakeJmsBroker;
+import com.devops.aws.test.externalprops.ExternalProperties;
+import com.devops.aws.test.externalprops.ExternalPropertiesMultiFile;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ExternalPropertiesMultiFile.class)
+
+public class ExternaPropertyMultiFileTest {
+
+    @Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropsSet() throws Exception {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Vero", fakeJmsBroker.getUser());
+        assertEquals("&%$)(*&#^!@!@#$", fakeJmsBroker.getPassword());
+    }
+}
---
+++ devops.aws/src/test/java/com/devops/aws/test/externalprops/ExternalPropertiesMultiFile.java
@@ -0,0 +1,28 @@
+package com.devops.aws.test.externalprops;
+
+import com.devops.aws.jms.FakeJmsBroker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource({"classpath:testing.properties", "classpath:encrypted-testing.properties"})
+public class ExternalPropertiesMultiFile {
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    public FakeJmsBroker fakeJmsBrokerMulti(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUrl(env.getProperty("vero.jms.server"));
+        fakeJmsBroker.setPort(env.getRequiredProperty("vero.jms.port", Integer.class));
+        fakeJmsBroker.setUser(env.getProperty("vero.jms.user"));
+        fakeJmsBroker.setPassword(env.getProperty("vero.jms.encrypted.password"));
+        return fakeJmsBroker;
+    }
+
+
+}
