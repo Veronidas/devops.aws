137bc082a82f4571512d3f8fc3439a77e683951b
Veronidas <veronabrkich@gmail.com>
2024-01-29 18:15:31 -0300

    test and project initial configuration

---
+++ devops.aws/src/test/java/com/devops/aws/externalproptest/ExternalPropertyTest.java
@@ -0,0 +1,29 @@
+package com.devops.aws.externalproptest;
+
+import com.devops.aws.jms.FakeJmsBroker;
+import com.devops.aws.test.externalprops.ExternalProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ExternalProperties.class)
+
+public class ExternalPropertyTest {
+
+    @Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropsSet() throws Exception {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Vero", fakeJmsBroker.getUser());
+        assertEquals("Baggins", fakeJmsBroker.getPassword());
+    }
+}
---
+++ devops.aws/src/test/java/com/devops/aws/test/externalprops/ExternalProperties.java
@@ -0,0 +1,47 @@
+package com.devops.aws.test.externalprops;
+
+import com.devops.aws.jms.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:testing.properties")
+public class ExternalProperties {
+
+    @Value("${vero.jms.server}")
+    String jmsServer;
+
+    @Value("${vero.jms.port}")
+    Integer jmsPort;
+
+    @Value("${vero.jms.user}")
+    String jmsUser;
+
+    @Value("${vero.jms.password}")
+    String jmsPassword;
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
+                = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUrl(jmsServer);
+        fakeJmsBroker.setPort(jmsPort);
+        fakeJmsBroker.setUser(jmsUser);
+        fakeJmsBroker.setPassword(jmsPassword);
+        return fakeJmsBroker;
+    }
+}
+
+
+
+
