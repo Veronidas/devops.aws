f677a9bc3f4ec9f69516e2eea8dd521315623013
Veronidas <veronabrkich@gmail.com>
2024-01-29 19:12:34 -0300

    Add multifilProperty Source refactor

--- devops.aws/src/test/java/com/devops/aws/test/externalprops/ExternalPropertiesMultiFile.java
+++ devops.aws/src/test/java/com/devops/aws/test/externalprops/ExternalPropertiesMultiFile.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource({"classpath:testing.properties", "classpath:encrypted-testing.properties"})
+@PropertySources({
+        @PropertySource("classpath:testing.properties"),
+        @PropertySource("classpath:encrypted-testing.properties")})
 public class ExternalPropertiesMultiFile {
 
     @Autowired
